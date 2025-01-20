6de171e017040624ae0a5fc3998a531e60dc71f6
marintremine <marin.tremine@gmail.com>
2025-01-20 15:17:15 +0100

    tp until question 5

--- README.md
+++ README.md
@@ -15,5 +15,64 @@ TODO
 
 TODO
 
+## Utilisation
+
+ditherpunk [OPTIONS] <INPUT> <OUTPUT> <SUBCOMMAND>
+
+ditherpunk.exe -- --help
+```bash
+Convertit une image en monochrome ou vers une palette réduite de couleurs.
+
+Positional Arguments:
+  input             le fichier d’entrée
+  output            le fichier de sortie (optionnel)
+
+Options:
+  --help, help      display usage information
+
+Commands:
+  seuil             Rendu de l’image par seuillage monochrome.  
+  palette           Rendu de l’image avec une palette contenant un nombre limité de couleurs
+```
+Exemple 
+    
+```bash
+cargo run -- input.png seuil
+```
+
+```bash
+cargo run -- input_image.png output_image.png palette --n-couleurs 4
+```
 
 
+Question 1 :
+Ouvrir une image depuis un fichier
+Pour ouvrir une image depuis un fichier, vous pouvez utiliser la méthode suivante :
+
+```rust
+use image::io::Reader as ImageReader;
+use image::DynamicImage;
+
+let img = ImageReader::open("image.png")?.decode()?;
+```
+
+DynamicImage
+Le type DynamicImage est une énumération de la crate image qui peut représenter différentes formes d'images. Il peut contenir des images en niveaux de gris, en RGB, en RGBA, etc. Cela permet de manipuler des images sans se soucier de leur format interne initial.
+
+Convertir en RGB8
+Pour convertir une DynamicImage en une image en mode RGB8, vous pouvez utiliser la méthode to_rgb8() :
+
+```rust
+let rgb_image = img.to_rgb8();
+```
+
+Question 2 :
+
+Sauver l'image au format PNG
+Pour sauver l'image obtenue au format PNG, vous pouvez utiliser la méthode save :
+
+```rust
+rgb_image.save("output.png")?;
+```
+
+Si l'image de départ avait un canal alpha (par exemple, une image en RGBA), la conversion en RGB8 via to_rgb8() va ignorer le canal alpha et ne conserver que les canaux rouge, vert et bleu. Cela signifie que toute information de transparence sera perdue dans l'image convertie.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,86 @@
+use argh::FromArgs;
+use image::io::Reader as ImageReader;
+use image::DynamicImage;
+use image::{Rgb, RgbImage};
+
+#[derive(Debug, Clone, PartialEq, FromArgs)]
+/// Convertit une image en monochrome ou vers une palette réduite de couleurs.
+struct DitherArgs {
+
+    /// le fichier d’entrée
+    #[argh(positional)]
+    input: String,
+
+    /// le fichier de sortie (optionnel)
+    #[argh(positional)]
+    output: Option<String>,
+
+    /// le mode d’opération
+    #[argh(subcommand)]
+    mode: Mode
+}
+
+#[derive(Debug, Clone, PartialEq, FromArgs)]
+#[argh(subcommand)]
+enum Mode {
+    Seuil(OptsSeuil),
+    Palette(OptsPalette),
+}
+
+#[derive(Debug, Clone, PartialEq, FromArgs)]
+#[argh(subcommand, name="seuil")]
+/// Rendu de l’image par seuillage monochrome.
+struct OptsSeuil {}
+
+
+#[derive(Debug, Clone, PartialEq, FromArgs)]
+#[argh(subcommand, name="palette")]
+/// Rendu de l’image avec une palette contenant un nombre limité de couleurs
+struct OptsPalette {
+
+    /// le nombre de couleurs à utiliser, dans la liste [NOIR, BLANC, ROUGE, VERT, BLEU, JAUNE, CYAN, MAGENTA]
+    #[argh(option)]
+    n_couleurs: usize
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: DitherArgs = argh::from_env();
+    let path_in = args.input;
+    println!("path_in: {}", path_in);
+    let path_out = args.output.unwrap_or("output/out.png".to_string());
+    println!("path_out: {}", path_out);
+    // sandbox
+    
+    let img = match ImageReader::open(path_in) {
+        Ok(reader) => reader.decode(),
+        Err(err) => {
+            eprintln!("Failed to open image: {}", err);
+            return;
+        }
+    };
+
+    // Convertir l'image en mode RGB8
+    let mut rgb_image = img.unwrap().to_rgb8();
+
+    // Parcourir tous les pixels de l'image
+    for (x, y, pixel) in rgb_image.enumerate_pixels_mut() {
+        // Passer un pixel sur deux en blanc
+        if (x + y) % 2 == 0 {
+            *pixel = Rgb([255, 255, 255]);
+        }
+    }
+
+    // Sauvegarder l'image convertie
+    rgb_image.save(path_out).unwrap();
+
+    //rgb_image.save("output_image.png")?;
+    println!("L'image a été convertie en mode RGB8 avec succès.");
+
+    // Accéder au pixel à la position (32, 52)
+    let pixel = rgb_image.get_pixel(32, 51);
+
+    // Afficher la couleur du pixel dans le terminal
+    println!("La couleur du pixel (32, 52) est : {:?}", pixel);
+
+
 }
